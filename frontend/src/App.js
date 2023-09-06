ec2ad04e5016b6edae9caaf87c3988af62d5952b
azrulwm <azrulazmi.wm@gmail.com>
2023-09-06 15:06:05 +0800

    Adding some css for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [items, setItems] = useState([]); // Store fetched items
   const [selectedItems, setSelectedItems] = useState([]);
   const [packages, setPackages] = useState([]);
-  console.log(process.env.REACT_APP_API_BASE_URL);
+
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/products/`)
       .then((response) => response.json())
@@ -20,21 +20,24 @@ function App() {
   };
 
   const handlePlaceOrder = () => {
-    fetch("/api/placeOrder", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: selectedItems }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setPackages(data.packages);
-      });
+    console.log(selectedItems);
+    // fetch("/api/placeOrder", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({ items: selectedItems }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setPackages(data.packages);
+    //   });
   };
 
   return (
     <div className="App">
       <ItemList items={items} onSelectItem={handleSelectItem} />
-      <button onClick={handlePlaceOrder}>Place order</button>
+      <button onClick={handlePlaceOrder} className="btn-primary">
+        Place order
+      </button>
       <OrderSummary packages={packages} />
     </div>
   );
--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,13 +1,31 @@
 const ItemList = ({ items, onSelectItem }) => {
   return (
-    <div>
+    <div className="max-w-xl mx-auto bg-white p-8 rounded-xl shadow-lg border border-gray-100">
+      <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+        Select Items
+      </h1>
       {items.map((item) => (
-        <div key={item.id}>
-          <input type="checkbox" onChange={() => onSelectItem(item.id)} />
-          {item.name} - ${item.price} - {item.weight}g
-        </div>
+        <label
+          key={item.id}
+          htmlFor={`item-${item.id}`}
+          className="flex mb-2 items-center justify-between p-6 border rounded-lg bg-gray-50 hover:bg-gray-100 transition-transform transform hover:scale-105 duration-300 cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            id={`item-${item.id}`}
+            className="form-checkbox h-6 w-6 text-blue-600 mr-4"
+            onChange={() => onSelectItem(item.id)}
+          />
+          <div className="flex-1 text-left text-gray-700 font-semibold">
+            <span className="block text-lg">{item.name}</span>
+            <span className="text-sm text-gray-500 mt-1">
+              ${item.price} - {item.weight}g
+            </span>
+          </div>
+        </label>
       ))}
     </div>
   );
 };
+
 export default ItemList;
