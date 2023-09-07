95196cbf66bd92906e7b59342d2e46fa887da754
azrulwm <azrulazmi.wm@gmail.com>
2023-09-07 12:01:18 +0800

    Improving the logic for creating order and splitting packages

--- backend/src/module/order/order.service.ts
+++ backend/src/module/order/order.service.ts
@@ -10,7 +10,7 @@ export class OrderService {
     return result;
   }
 
-  createPackages(items: any[]) {
+  createPackages(items: Product[]) {
     let totalCost = 0;
     let totalWeight = 0;
     const packages = [];
@@ -32,14 +32,55 @@ export class OrderService {
       totalCost += item.price;
       totalWeight += item.weight;
     }
+
     if (currentPackage.items.length) packages.push(currentPackage);
 
-    // Distribute weights if multiple packages and total cost is above $250
-    if (packages.length > 1 && totalCost > 250) {
-      const avgWeight = totalWeight / packages.length;
-      packages.forEach((pkg) => {
-        pkg.weight = avgWeight;
-      });
+    // Balance weights if multiple packages
+    if (packages.length > 1) {
+      let avgWeight = totalWeight / packages.length;
+
+      console.log('Balancing packages weight');
+
+      let maxIterations = 200;
+      let changesMade = true;
+
+      // To prevent infinite while loop
+      while (changesMade && maxIterations > 0) {
+        console.log('Sorting....');
+
+        changesMade = false;
+        maxIterations--;
+
+        const sortedPackages = [...packages].sort(
+          (a, b) => a.weight - b.weight,
+        );
+        const lightest = sortedPackages[0];
+        const heaviest = sortedPackages[sortedPackages.length - 1];
+
+        if (lightest.weight < avgWeight && heaviest.weight > avgWeight) {
+          for (const item of heaviest.items) {
+            if (lightest.cost + item.price < 250) {
+              lightest.items.push(item);
+              lightest.weight += item.weight;
+              lightest.cost += item.price;
+
+              const index = heaviest.items.indexOf(item);
+              if (index > -1) {
+                heaviest.items.splice(index, 1);
+                heaviest.weight -= item.weight;
+                heaviest.cost -= item.price;
+              }
+
+              changesMade = true;
+              break;
+            }
+          }
+        }
+      }
+
+      if (maxIterations <= 0) {
+        console.warn('Weight balancing reached max iterations!');
+      }
     }
 
     return packages;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,17 +20,28 @@ function App() {
   };
 
   const handlePlaceOrder = () => {
-    console.log(selectedItems);
-
+    console.log(selectedItems.length);
     fetch(`${process.env.REACT_APP_API_BASE_URL}/orders`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ids: selectedItems }),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
         // setPackages(data.packages);
+      })
+      .catch((error) => {
+        console.log(
+          "There was a problem with the fetch operation:",
+          error.message
+        );
       });
   };
 
